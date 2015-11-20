00374571796bfdb91decb94d467a787d809be579
Ernest Leitch <ernestleitch@Ernests-MacBook.local>
2015-11-20 07:36:37 -0800

    forgot tags

--- keys.php
+++ keys.php
@@ -1,5 +1,7 @@
+<?php 
 define("RDIO_CLIENT_SECRET", "rdio secret");
 define("RDIO_TOKEN_URI", "https://services.rdio.com/oauth2/token");
 
 define("SPOTIFY_CLIENT_SECRET", "spotify secret");
 define("SPOTIFY_TOKEN_URI", "https://accounts.spotify.com/api/token");
+?>
